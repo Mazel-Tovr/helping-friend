18567091843efaf447709769fdf64469cdc68a3a
Roma <roma.ivanov1999@mail.ru>
2021-05-17 23:46:19 +0300

    add first task

---
+++ secondSemak/FirstTask/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace FirstTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.change = new System.Windows.Forms.Label();
+            this.pay = new System.Windows.Forms.Button();
+            this.po = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ps = new System.Windows.Forms.ListBox();
+            this.total = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(241, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Покупка компьютера";
+            // 
+            // sum
+            // 
+            this.sum.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sum.Location = new System.Drawing.Point(199, 369);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(107, 22);
+            this.sum.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Внесенная сумма";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(361, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Сдача: ";
+            // 
+            // change
+            // 
+            this.change.AutoSize = true;
+            this.change.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.change.Location = new System.Drawing.Point(453, 369);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(24, 25);
+            this.change.TabIndex = 5;
+            this.change.Text = "0";
+            // 
+            // pay
+            // 
+            this.pay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pay.Location = new System.Drawing.Point(562, 355);
+            this.pay.Name = "pay";
+            this.pay.Size = new System.Drawing.Size(136, 47);
+            this.pay.TabIndex = 6;
+            this.pay.Text = "Оплата";
+            this.pay.UseVisualStyleBackColor = true;
+            this.pay.Click += new System.EventHandler(this.pay_Click);
+            // 
+            // po
+            // 
+            this.po.HideSelection = false;
+            this.po.Location = new System.Drawing.Point(312, 301);
+            this.po.Name = "po";
+            this.po.Size = new System.Drawing.Size(386, 51);
+            this.po.TabIndex = 7;
+            this.po.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(307, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Список по";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FirstTask.Properties.Resources.ps;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 202);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ps
+            // 
+            this.ps.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ps.FormattingEnabled = true;
+            this.ps.ItemHeight = 25;
+            this.ps.Location = new System.Drawing.Point(312, 41);
+            this.ps.Name = "ps";
+            this.ps.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.ps.Size = new System.Drawing.Size(386, 229);
+            this.ps.TabIndex = 9;
+            this.ps.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.total.Location = new System.Drawing.Point(655, 273);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(24, 25);
+            this.total.TabIndex = 11;
+            this.total.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(443, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(190, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Общая стоимость";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 425);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ps);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.po);
+            this.Controls.Add(this.pay);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label change;
+        private System.Windows.Forms.Button pay;
+        private System.Windows.Forms.ListView po;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox ps;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ secondSemak/FirstTask/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstTask
+{
+    public partial class Form1 : Form
+    {
+        private List<PS> computes = new List<PS>() {
+            new PS("Mac",2000 , "Mac OS"), new PS("MSI",1000,"Windows"), new PS("hyperpc", 500,"Linux"),new PS("Gameing ps",500,"FreeBsd")
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            computes.ForEach(e=>this.ps.Items.Add(e));
+        }
+
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pay_Click(object sender, EventArgs e)
+        {
+            double input = double.Parse(sum.Text);
+            double totalsum = double.Parse(total.Text);
+            if(totalsum > input)
+            {
+                MessageBox.Show("Недостаточно денег");
+            } else
+            {
+                MessageBox.Show("Покупка совершена. Сдача:" + (input - totalsum));
+                change.Text = (input - totalsum).ToString();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<PS> computes1 = new List<PS>();
+            var enumerator = ps.SelectedItems.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                computes1.Add((PS)enumerator.Current);
+            }
+            double sum = 0;
+            po.Items.Clear();
+            computes1.ForEach(z => { 
+                sum += z.price;
+                po.Items.Add(z.po);
+            });
+            total.Text =  sum.ToString();
+        }
+    }
+
+    struct PS
+    {
+        public PS(String name, double price, string po)
+        {
+            this.name = name;
+            this.price = price;
+            this.po = po;
+        }
+        public string name;
+        public double price;
+        public string po;
+
+        public override string ToString()
+        {
+            return name + " " + price;
+        }
+    }
+
+   
+}
